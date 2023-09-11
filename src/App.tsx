15944399bed21135e77d9027f53fbd42480dc71d
Baptiste Nonat <estitpab@gmail.com>
2023-09-11 10:45:58 +0200

    Add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import styles from './App.module.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isExpectedResult, setIsExpectedResult] = useState(false);
+  const EXPECTED_VALUES = {
+    value1: "A",
+    value2: "B",
+    value3: "C",
+    value4: "D",
+    value5: "E",
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement | EventTarget>) => {
+    e.preventDefault();
+
+    if (e.target instanceof HTMLFormElement) {
+      const form = e.target;
+      const formData = new FormData(form);
+      const formJson = Object.fromEntries(formData.entries());
+      console.log(formJson);
+
+      setIsExpectedResult(
+        formJson.value1 === EXPECTED_VALUES.value1 &&
+          formJson.value2 === EXPECTED_VALUES.value2 &&
+          formJson.value3 === EXPECTED_VALUES.value3 &&
+          formJson.value4 === EXPECTED_VALUES.value4 &&
+          formJson.value5 === EXPECTED_VALUES.value5
+      );
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={styles.formWrapper}>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.inputsWrapper}>
+          <input required name="value1" minLength={1} maxLength={2} size={2} />
+          <input required name="value2" minLength={1} maxLength={2} size={2} />
+          <input required name="value3" minLength={1} maxLength={2} size={2} />
+          <input required name="value4" minLength={1} maxLength={2} size={2} />
+          <input required name="value5" minLength={1} maxLength={2} size={2} />
+        </div>
+
+        <button type="submit">Valider le code</button>
+      </form>
+      {isExpectedResult && <p style={{ color: "green" }}>Bravo !</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
